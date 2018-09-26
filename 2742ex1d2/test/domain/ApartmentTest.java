fa5d98ce0ce1acc41d24bfc54d8d15ec695d0a23
mswanson-mscst <mswanson@southeastmn.edu>
2018-09-26 17:18:29 -0500

    Added 2742ex1d2 Apt, Invoice unit tests

---
+++ 2742ex1d2/test/domain/ApartmentTest.java
@@ -0,0 +1,192 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class ApartmentTest {
+    Apartment apt = null;
+
+    @BeforeEach
+    void setUp() {
+        this.apt = new Apartment();
+    }
+
+    @Test
+    void setApartmentIdBelowRange() {
+        assertNotEquals(this.apt.setApartmentId(99), "");
+    }
+
+    @Test
+    void setApartmentIdMinimum() {
+        assertEquals(this.apt.setApartmentId(100), "");
+    }
+
+    @Test
+    void setApartmentIdMaximum() {
+        assertEquals(this.apt.setApartmentId(199), "");
+    }
+
+    @Test
+    void setApartmentAboveRange() {
+        assertNotEquals(this.apt.setApartmentId(200), "");
+    }
+
+    @Test
+    void setApartmentNumNull() {
+        assertNotEquals(this.apt.setApartmentNum(null), "");
+    }
+
+    @Test
+    void setApartmentNumEmptyString() {
+        assertNotEquals(this.apt.setApartmentNum(""), "");
+    }
+
+    @Test
+    void setApartmentNumMinLength() {
+        assertEquals(this.apt.setApartmentNum("1"), "");
+    }
+
+    @Test
+    void setApartmentNumMaxLength() {
+        assertEquals(this.apt.setApartmentNum("1111"), "");
+    }
+
+    @Test
+    void setApartmentNumTooLong() {
+        assertNotEquals(this.apt.setApartmentNum("11111"), "");
+    }
+
+    @Test
+    void setSquareFeetTooSmall() {
+        assertNotEquals(this.apt.setSquareFeet(199), "");
+    }
+
+    @Test
+    void setSquareFeetMinimum() {
+        assertEquals(this.apt.setSquareFeet(200), "");
+    }
+
+    @Test
+    void setSquareFeetMaximum() {
+        assertEquals(this.apt.setSquareFeet(2000), "");
+    }
+
+    @Test
+    void setSquareFeetTooLarge() {
+        assertNotEquals(this.apt.setSquareFeet(2001), "");
+    }
+
+    @Test
+    void setBathroomsToLow() {
+        assertNotEquals(this.apt.setBathrooms(0), "");
+    }
+
+    @Test
+    void setBathroomsMinimum() {
+        assertEquals(this.apt.setBathrooms(1), "");
+    }
+
+    @Test
+    void setBathroomsMaximum() {
+        assertEquals(this.apt.setBathrooms(3), "");
+    }
+
+    @Test
+    void setBathroomsTooHigh() {
+        assertNotEquals(this.apt.setBathrooms(4), "");
+    }
+
+    @Test
+    void setPriceTooLow() {
+        assertNotEquals(this.apt.setPrice(99.99), "");
+    }
+
+    @Test
+    void setPriceTooHigh() {
+        assertNotEquals(this.apt.setPrice(10000.00), "");
+    }
+
+    @Test
+    void getAdministrator() {
+        Person person = new Person(101, "FirstName", "LastName", "UserName");
+        this.apt.setAdministrator(person);
+        assertEquals(this.apt.getAdministrator().getPersonId(), 101);
+    }
+
+    @Test
+    void getTenant() {
+        Person person = new Person(101, "FirstName", "LastName", "UserName");
+        this.apt.setTenant(person);
+        assertEquals(this.apt.getTenant().getPersonId(), 101);
+    }
+
+    @Test
+    void getInvoice() {
+        Invoice invoice1 = new Invoice(1,
+            LocalDateTime.of(2019, Month.OCTOBER, 15, 0, 0),
+            LocalDateTime.of(2019, Month.NOVEMBER, 1, 0, 0),
+            this.apt);
+        invoice1.addLineItem( new LineItem(5.0, "description5") );
+        invoice1.addLineItem( new LineItem(6.0, "description6") );
+        invoice1.addLineItem( new LineItem(7.0, "description7") );
+        invoice1.addLineItem( new LineItem(8.0, "description8") );
+        this.apt.addInvoice(invoice1);
+
+        Invoice invoice2 = this.apt.getInvoice(0);
+
+        assertEquals(invoice1, invoice2);               // invoice1 and invoice2 must be equal
+        assertFalse(invoice1 == invoice2);    // invoice2 must be copy of invoice1
+
+        // Test bad index = 1
+        Invoice invoice3 = this.apt.getInvoice(1);
+        assertTrue(invoice3 == null);
+    }
+
+    @Test
+    void getInvoices() {
+        ArrayList<Invoice> invoices = new ArrayList<Invoice>();
+        invoices.add( new Invoice(1,
+                LocalDateTime.of(2019, Month.SEPTEMBER, 1, 0, 0),
+                LocalDateTime.of(2019, Month.SEPTEMBER, 11, 0, 0),
+                this.apt));
+        invoices.add( new Invoice(2,
+                LocalDateTime.of(2019, Month.OCTOBER, 1, 0, 0),
+                LocalDateTime.of(2019, Month.OCTOBER, 11, 0, 0),
+                this.apt));
+        for (Invoice invoice : invoices) {
+            this.apt.addInvoice(invoice);
+        }
+
+        for (int i = 0; i < invoices.size(); i++) {
+            Invoice invoice1 = invoices.get(i);
+            Invoice invoice2 = this.apt.getInvoice(i);
+            assertEquals(invoice1, invoice2);               // invoice1 must equal invoice2
+            assertFalse(invoice1 == invoice2);    // invoice1 must be copy of invoice2
+        }
+    }
+
+    @Test
+    void removeInvoice() {
+        Invoice invoice1 = new Invoice(1,
+                LocalDateTime.of(2019, Month.SEPTEMBER, 1, 0, 0),
+                LocalDateTime.of(2019, Month.SEPTEMBER, 11, 0, 0),
+                this.apt);
+        this.apt.addInvoice(0, invoice1);
+
+        Invoice invoice2 = this.apt.removeInvoice(0);
+        assertEquals(invoice1, invoice2);               // invoice1 and invoice2 must be equal
+        assertFalse(invoice1 == invoice2);    // invoice2 must be copy of invoice1
+
+        // Test bad index = 0
+        Invoice invoice3 = this.apt.removeInvoice(0);
+        assertTrue(invoice3 == null);
+    }
+}
---
+++ 2742ex1d2/test/domain/InvoiceTest.java
@@ -0,0 +1,173 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvoiceTest {
+
+    Invoice invoice1;
+
+    @BeforeEach
+    void setUp() {
+//        this.invoice1 = new Invoice(1, new GDate(), new GDate());
+        this.invoice1 = new Invoice(1,
+                LocalDateTime.of(2019, Month.SEPTEMBER, 1, 0, 0),
+                LocalDateTime.of(2019, Month.SEPTEMBER, 11, 0, 0),
+                null);
+    }
+
+    @Test
+    void copyConstructor() {
+        this.invoice1.addLineItem(new LineItem(1.0, "description1"));
+        this.invoice1.addLineItem(new LineItem(2.0, "description2"));
+        this.invoice1.addLineItem(new LineItem(3.0, "description3"));
+        Invoice invoice2 = new Invoice(this.invoice1);
+        assertEquals(this.invoice1.getInvoiceId(), invoice2.getInvoiceId());
+        assertEquals(this.invoice1.getStatus(), invoice2.getStatus());
+//        assertEquals(this.invoice1.getInvoiceDate().julianDay(), invoice2.getInvoiceDate().julianDay());
+//        assertEquals(this.invoice1.getDueDate().julianDay(), invoice2.getDueDate().julianDay());
+//        assertFalse(this.invoice1.getInvoiceDate() == invoice2.getInvoiceDate());
+//        assertFalse(this.invoice1.getDueDate() == invoice2.getDueDate());
+        assertTrue(this.invoice1.getInvoiceDate().equals(invoice2.getInvoiceDate()));       // ex1d
+        assertTrue(this.invoice1.getDueDate().equals(invoice2.getDueDate()));               // ex1d
+
+        // ex1c: test lineItems
+        ArrayList<LineItem> lineItems1 = this.invoice1.getLineItems();
+        ArrayList<LineItem> lineItems2 = invoice2.getLineItems();
+        assertEquals(lineItems1.size(), lineItems2.size());
+        int i = 0;
+        LineItem lineItem2 = null;
+        for (LineItem lineItem1 : lineItems1) {
+            lineItem2 = lineItems2.get(i);
+            assertEquals(lineItem1.getLineItemId(), lineItem2.getLineItemId());
+            assertEquals(lineItem1.getAmount(), lineItem2.getAmount());
+            assertEquals(lineItem1.getDescription(), lineItem2.getDescription());
+            assertFalse(lineItem1 == lineItem2);
+            i++;
+        }
+    }
+
+    @Test
+    void copy() {
+        // Ex1c: add LineItems
+        this.invoice1.addLineItem(new LineItem(1.0, "description1"));
+        this.invoice1.addLineItem(new LineItem(2.0, "description2"));
+        this.invoice1.addLineItem(new LineItem(3.0, "description3"));
+
+        Invoice invoice2 = this.invoice1.copy();
+        assertEquals(this.invoice1.getInvoiceId(), invoice2.getInvoiceId());
+        assertEquals(this.invoice1.getStatus(), invoice2.getStatus());
+//        assertEquals(this.invoice1.getInvoiceDate().julianDay(), invoice2.getInvoiceDate().julianDay());
+//        assertEquals(this.invoice1.getDueDate().julianDay(), invoice2.getDueDate().julianDay());
+//        assertFalse(this.invoice1.getInvoiceDate() == invoice2.getInvoiceDate());
+//        assertFalse(this.invoice1.getDueDate() == invoice2.getDueDate());
+        assertTrue(this.invoice1.getInvoiceDate().equals(invoice2.getInvoiceDate()));       // ex1d
+        assertTrue(this.invoice1.getDueDate().equals(invoice2.getDueDate()));               // ex1d
+
+        // ex1c: test lineItems
+        ArrayList<LineItem> lineItems1 = this.invoice1.getLineItems();
+        ArrayList<LineItem> lineItems2 = invoice2.getLineItems();
+        assertEquals(lineItems1.size(), lineItems2.size());
+        int i = 0;
+        LineItem lineItem2 = null;
+        for (LineItem lineItem1 : lineItems1) {
+            lineItem2 = lineItems2.get(i);
+            assertEquals(lineItem1.getLineItemId(), lineItem2.getLineItemId());
+            assertEquals(lineItem1.getAmount(), lineItem2.getAmount());
+            assertEquals(lineItem1.getDescription(), lineItem2.getDescription());
+            assertFalse(lineItem1 == lineItem2);
+            i++;
+        }
+    }
+
+//    @Test
+//    void addLineItem() {
+//    }
+
+    @Test
+    void removeLineItem() {
+        LineItem lineItem1 = new LineItem(1.0, "description1");
+
+        // test Invoice.removeLineItem(int index)
+        this.invoice1.addLineItem(lineItem1);
+        LineItem removedLineItem = this.invoice1.removeLineItem(0);
+        assertEquals(lineItem1, removedLineItem);
+        assertFalse(lineItem1 == removedLineItem);
+        // test removing from empty ArrayList
+        removedLineItem = this.invoice1.removeLineItem(0);
+        assertEquals(null, removedLineItem);
+
+        // test Invoice.removeLineItem(LineItem lineItem)
+        this.invoice1.addLineItem(lineItem1);
+        removedLineItem = this.invoice1.removeLineItem(lineItem1);
+        assertEquals(lineItem1, removedLineItem);
+        assertFalse(lineItem1 == removedLineItem);
+        // test removing from empty ArrayList
+        removedLineItem = this.invoice1.removeLineItem(lineItem1);
+        assertEquals(null, removedLineItem);
+    }
+
+    @Test
+    void total() {
+        this.invoice1.addLineItem( new LineItem(1.0, "description1") );
+        this.invoice1.addLineItem( new LineItem(2.0, "description2") );
+        this.invoice1.addLineItem( new LineItem(3.0, "description3") );
+        this.invoice1.addLineItem( new LineItem(4.0, "description4") );
+
+        assertEquals(10.0, this.invoice1.total());
+
+        this.invoice1.removeLineItem(0);
+        this.invoice1.removeLineItem(0);
+        this.invoice1.removeLineItem(0);
+        this.invoice1.removeLineItem(0);
+        LineItem removedLineItem = this.invoice1.removeLineItem(0);
+        assertEquals(null, removedLineItem);
+    }
+
+    @Test
+    void toStringTest() {
+        // test Invoice.toShortString() and Invoice.toString()
+        assertEquals("status: 1, 2019/09/01", this.invoice1.toShortString().substring(6));
+        assertEquals(
+                "status=1, invoiceDate=2019/09/01, dueDate=2019/09/11}",
+                this.invoice1.toString().substring(24));
+    }
+
+    @Test
+    void getLineItem() {
+        LineItem lineItem1 = new LineItem(1.0, "description1");
+        this.invoice1.addLineItem(lineItem1);
+        LineItem lineItem2 = this.invoice1.getLineItem(0);
+        assertEquals(lineItem1, lineItem2);
+        assertFalse(lineItem1 == lineItem2);
+
+        // Test bad index = 1
+        LineItem lineItem3 = this.invoice1.getLineItem(1);
+        assertTrue(lineItem3 == null);
+    }
+
+    @Test
+    void getLineItems() {
+        ArrayList<LineItem> lineItems = new ArrayList<LineItem>();
+        lineItems.add( new LineItem(1.0, "description1") );
+        lineItems.add( new LineItem(2.0, "description2") );
+        lineItems.add( new LineItem(3.0, "description3") );
+        lineItems.add( new LineItem(4.0, "description4") );
+        for (LineItem lineItem : lineItems) {
+            this.invoice1.addLineItem(lineItem);
+        }
+
+        for (int i = 0; i < lineItems.size(); i++) {
+            LineItem lineItem1 = lineItems.get(i);
+            LineItem lineItem2 = this.invoice1.getLineItem(i);
+            assertEquals(lineItem1, lineItem2);               // lineItem1 must equal lineItem2
+            assertFalse(lineItem1 == lineItem2);    // lineItem2 must be copy of lineItem1
+        }
+    }
+}
